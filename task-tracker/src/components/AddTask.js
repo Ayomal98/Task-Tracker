d06f62a07da84f6e6bf191e40ad0911282cfe8a0
Ayomal98 <ayomalpraveen33@gmail.com>
2021-01-24 13:04:15 +0530

    added the use state hooks for form handler

--- task-tracker/src/components/AddTask.js
+++ task-tracker/src/components/AddTask.js
@@ -1,20 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const AddTask = () => {
+    const [text,setText]=useState('')
+    const [date,setDate]=useState('')
+    const [reminder,setReminder]=useState(false)
+
+    const textHandler=(text)=>{
+        setText(text)
+    }
+    
+    const dateHandler=(date)=>{
+        setDate(date)
+    }
+    
+    const reminderHandler=()=>{
+        setReminder(!reminder)
+    }
     
     return (
         <form className='add-form'>
             <div className="form-control">
                 <label>Task</label>
-                <input type='text' placeholder='Add Task' />
+                <input type='text' placeholder='Add Task' value={text} onChange={(e)=>textHandler(e.target.value)}/>
             </div>
             <div className="form-control">
                 <label>Date</label>
-                <input type='text' placeholder='Add Date' />
+                <input type='text' placeholder='Add Date' value={date} onChange={(e)=>dateHandler(e.target.value)}/>
             </div>
             <div className="form-control form-control-check">
                 <label>Set Reminder</label>
-                <input type='checkbox' />
+                <input type='checkbox' onClick={reminderHandler} />
             </div>
             <input type="submit" value="Add" className="btn btn-block"/>
         </form>
